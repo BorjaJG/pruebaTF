b1f242626359da68b90ce584931ed2a014cc23ea
Borja <borjajg02@gmail.com>
2024-04-18 10:21:36 +0200

    Se creado la estructura de presentación .
    Main.java -> Actulizado

--- src/main/java/com/iesam/digitallibrary/user/data/UserDataRepository.java
+++ src/main/java/com/iesam/digitallibrary/user/data/UserDataRepository.java
@@ -3,11 +3,23 @@ package com.iesam.digitallibrary.user.data;
 import com.iesam.digitallibrary.user.data.local.UserFileLocalDataSource;
 import com.iesam.digitallibrary.user.domain.User;
 import com.iesam.digitallibrary.user.domain.UserRepository;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserDataRepository implements UserRepository {
 
+    public UserFileLocalDataSource userFileLocalDataSource = new UserFileLocalDataSource();
+    public final Map<String, User> userMap = new HashMap<>();
+    public final ArrayList<User> users = new ArrayList<>();
+    public static UserRepository instance=null;
+    public static UserDataRepository newInstance(){
+        if(instance==null){
+            instance=new UserDataRepository();
+        }
+        return (UserDataRepository) instance;
+    }
 
-    UserFileLocalDataSource userFileLocalDataSource = new UserFileLocalDataSource();
 
     @Override
     public boolean save(User user) {
